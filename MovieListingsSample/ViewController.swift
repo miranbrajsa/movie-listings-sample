3ddf5cfec23dbaa4cec1ab5e37da1f8df546f8f3
Miran Brajsa <miran.brajsa@gmail.com>
2023-11-14 09:03:29 +0100

    Initial project commit.

---
+++ MovieListingsSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListingsSample
+//
+//  Created by Miran Brajsa on 14.11.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
